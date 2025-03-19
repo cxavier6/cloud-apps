27aabf1a7c556fe174ba6890ca3aa490620159b4
cxavier6 <cxavier.reis@gmail.com>
2025-03-19 11:01:27 -0300

    add eks module files

---
+++ terraform/modules/eks/iam.tf
@@ -0,0 +1,65 @@
+# Create IAM Role for EKS Cluster and node group
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = var.iam.eks_cluster_role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_eks_access_policy_association" "this" {
+  cluster_name = var.eks_cluster.cluster_name
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = "arn:aws:iam::547886934166:user/camila"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
+resource "aws_iam_role" "eks_node_group_role" {
+  name = var.eks_cluster.node_group.role_name
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group_role.name
+}
---
+++ terraform/modules/eks/variables.tf
@@ -0,0 +1,55 @@
+variable "eks_cluster" {
+  type = object({
+    cluster_name                        = optional(string, "cloud-apps")
+    enabled_cluster_log_types           = optional(list(string), ["api", "audit", "authenticator", "controllerManager", "scheduler"])
+    access_config_authentication_mode   = optional(string, "API_AND_CONFIG_MAP")
+    node_group = optional(object({
+      name              = optional(string, "cloud-apps-ng")
+      role_name         = optional(string, "cloud-apps-ng-role")
+      ami_type          = optional(string, "BOTTLEROCKET_x86_64")
+      instance_types    = optional(list(string), ["t3a.medium"])
+      capacity_type     = optional(string, "ON_DEMAND")
+      scaling_config    = optional(object({
+        desired_size    = optional(number, 2)
+        max_size        = optional(number, 2)
+        min_size        = optional(number, 2)
+      }), {
+        desired_size = 2
+        max_size     = 2
+        min_size     = 2
+      })
+    }), {
+      name              = "cloud-apps-ng"
+      role_name         = "cloud-apps-ng-role"
+      ami_type          = "BOTTLEROCKET_x86_64"
+      instance_types    = ["t3.medium"]
+      capacity_type     = "ON_DEMAND"
+    })
+  })
+
+  default = {}
+}
+
+variable "iam" {
+    type = object({
+        eks_cluster_role_name = optional(string, "eks-cluster-role")
+    })
+
+    default = {}
+}
+
+variable "vpc_id" {
+  description = "The VPC ID where the EKS cluster will be deployed"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of private subnets for the EKS cluster"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Default tags for resources"
+  type        = map(string)
+  default     = {}
+}
