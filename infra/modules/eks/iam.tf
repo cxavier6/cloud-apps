2fc304b6b655d980466347ccc14beda156303d8c
cxavier6 <cxavier.reis@gmail.com>
2025-03-20 14:18:54 -0300

    add eks managed addons and its permissions

--- infra/modules/eks/iam.tf
+++ infra/modules/eks/iam.tf
@@ -65,3 +65,73 @@ resource "aws_iam_role_policy_attachment" "eks_node_group-AmazonEC2ContainerRegi
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_node_group_role.name
 }
+
+# OIDC Provider
+data "tls_certificate" "oidc" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks_oidc" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.oidc.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+# IAM Role for EBS CSI Driver
+resource "aws_iam_role" "ebs_csi_role" {
+  name = var.eks_cluster.ebs_csi_role
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks_oidc.arn
+        }
+        Condition = {
+          StringEquals = {
+            "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Policy attachment for EBS CSI Driver
+resource "aws_iam_policy_attachment" "ebs_csi_policy" {
+  name       = "AmazonEKS_EBS_CSI_PolicyAttachment"
+  roles      = [aws_iam_role.ebs_csi_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+# IAM Role for VPC CNI
+data "aws_iam_policy_document" "vpc_cni_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks_oidc.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "vpc_cni_role" {
+  name               = "AmazonEKS_VPC_CNI_Role"
+  assume_role_policy = data.aws_iam_policy_document.vpc_cni_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "vpc_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.vpc_cni_role.name
+}
