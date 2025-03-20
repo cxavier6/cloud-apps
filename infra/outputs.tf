90ac91da6df53cbb3804e017df3d1d869abf2f98
cxavier6 <cxavier.reis@gmail.com>
2025-03-20 14:19:22 -0300

    add helm and k8s providers

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -17,3 +17,20 @@ output "private_subnets" {
   description = "The private subnets"
   value = module.vpc.private_subnets
 }
+
+output "cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+  sensitive   = true
+}
--- infra/providers.tf
+++ infra/providers.tf
@@ -5,3 +5,25 @@ provider "aws" {
         role_arn     = "arn:aws:iam::547886934166:role/TerraformRole-ca0b4b89-085b-462d-955e-08311cd335fb"
     }
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
+  }
+}
+
+provider "kubernetes" {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
+}
